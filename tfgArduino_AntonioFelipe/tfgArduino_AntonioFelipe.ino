1381f6cfd4daf3a53abc0472baa1535a259be1d0
Antonio Felipe Rojo <antonio.felipe97@gmail.com>
2021-12-04 12:23:50 +0100

    muchos cambios

--- tfgArduino_AntonioFelipe/tfgArduino_AntonioFelipe.ino
+++ tfgArduino_AntonioFelipe/tfgArduino_AntonioFelipe.ino
@@ -1,5 +1,7 @@
+
 ////////////////
 //////
+#include <DNSServer.h>
 #include <WiFi.h>
 #include <WiFiAP.h>
 #include <ESPmDNS.h>
@@ -8,7 +10,7 @@ const char *ss = "Arduino";
 const char *pass = "abcd1234";
 
 //////
-#include "FS.h"
+#include "FS.h" //Para usar File
 #include "ArduinoJson.h"
 
 
@@ -16,7 +18,7 @@ const char *pass = "abcd1234";
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
 
-byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+//byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 IPAddress server_addr(192,168,1,39);  // IP of the MySQL *server* here
 char user[] = "antonio";              // MySQL user login username
@@ -33,9 +35,18 @@ char passwordDB[] = "password";        // MySQL user login password
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+DNSServer dnsServer;
+
+int conf_distance;
+int conf_co2_mid;
+int conf_co2_max;
+String conf_dni;
+boolean semaforo;
+
+TaskHandle_t TaskCheckConnections;
 
 String lectura(const String& var){
-  //Lectura fichero JSON
+  /*//Lectura fichero JSON
   File file = SPIFFS.open("/configuracion.json","r");
   
   DynamicJsonDocument doc(1024);                         //Memory pool
@@ -45,16 +56,24 @@ String lectura(const String& var){
     Serial.println(error.f_str());
     return String();
   }
-  file.close();
+  file.close();*/
+
+  DynamicJsonDocument doc = readConFile();
 
   if(var == "SSID"){
-    const char* value = doc["ssid"];
-    return value;
+    //const char* value = doc["ssid"];
+    //const char* value = WiFi.SSID();
+    if(WiFi.SSID() != ""){
+          return "Conectado actualmente a: " + WiFi.SSID();
+    }
+    else{
+      return "El dispositivo no está conectado a ninguna red WiFi";
+    }
   }
-  if(var == "PASSWORD"){
-    const char* value = doc["password"];
-    return value;
-  }
+  //if(var == "PASSWORD"){
+  //  const char* value = doc["password"];
+  //  return value;
+  //}
   if(var == "CO2_MAX"){
     const char* value = doc["co2_max"];
     return value;
@@ -71,6 +90,18 @@ String lectura(const String& var){
     const char* value = doc["dni"];
     return value;
   }
+  if(var == "NOMBRE"){
+    const char* value = doc["nombre"];
+    return value;
+  }
+  if(var == "APELLIDOS"){
+    const char* value = doc["apellidos"];
+    return value;
+  }
+  if(var == "ALU_PASSWORD"){
+    const char* value = doc["alu_password"];
+    return value;
+  }
   
   return String();
 }
@@ -79,8 +110,10 @@ String modifyWifi(String ssid, String password){
     doc["ssid"] = ssid;
     doc["password"] = password;
 
+    semaforo = false;
+    WiFi.disconnect(true);
     writeConFile(doc);
-    
+    connectWifi();
     return String();
 }
 String modifyLimits(String co2_max, String co2_mid, String distance){
@@ -88,16 +121,29 @@ String modifyLimits(String co2_max, String co2_mid, String distance){
     doc["co2_max"] = co2_max;
     doc["co2_mid"] = co2_mid;
     doc["distance"] = distance;
-
+    
+    conf_distance = distance.toInt();
+    conf_co2_max = co2_max.toInt();
+    conf_co2_mid = co2_mid.toInt();
+    
     writeConFile(doc);
     
     return String();
 }
-String modifyStudent(String dni){
+String modifyStudent(String dni, String nombre, String apellidos, String password){
     DynamicJsonDocument doc = readConFile();
     doc["dni"] = dni;
+    doc["nombre"] = nombre;
+    doc["apellidos"] = apellidos;
+    doc["alu_password"] = password;
 
+    updateAlumnoDB(dni, nombre, apellidos, password, conf_dni);
+    conf_dni = dni;
+    
     writeConFile(doc);
+
+    createAccessPoint();
+    startDNS();
     
     return String();
 }
@@ -122,7 +168,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
   OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 // '1200px-LogoUCLM', 128x64px
-const unsigned char myBitmap [] PROGMEM = {
+const unsigned char logoUCLM [] PROGMEM = {
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
@@ -189,6 +235,12 @@ const unsigned char myBitmap [] PROGMEM = {
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
 };
 
+// 'database_icon', 10x10px
+const unsigned char database_icon [] PROGMEM = {
+  0x3f, 0x00, 0x7f, 0x80, 0x7f, 0x80, 0x3f, 0x00, 0x40, 0x80, 0x7f, 0x80, 0x3f, 0x00, 0x40, 0x80, 
+  0x7f, 0x80, 0x3f, 0x00
+};
+
 // Sensor CO2
 #define PIN_SENSOR 34
 
@@ -200,110 +252,30 @@ const unsigned char myBitmap [] PROGMEM = {
 
 Simple_HCSR04 *sensor;
 
+////// Captive Portal
+#include <AsyncTCP.h>
+
+class CaptiveRequestHandler : public AsyncWebHandler {
+public:
+  CaptiveRequestHandler() {}
+  virtual ~CaptiveRequestHandler() {}
+
+  bool canHandle(AsyncWebServerRequest *request){
+    //request->addInterestingHeader("ANY");
+    return true;
+  }
+
+  void handleRequest(AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html","text/html", false); 
+  }
+};
+
+/////
+
 void setup() {
   Serial.begin(115200); //Para hacer debug
 
-  ////////////////////////
-
-  // Initialize SPIFFS
-  if(!SPIFFS.begin(true)){
-    Serial.println("Error al montar el sistema de ficheros SPIFFS");
-    return;
-  }
-  
-  createAccessPoint();
-  
-
-
-                           //Memory pool
-  
-  // Connect to Wi-Fi
-  //const char* ssid = doc["ssid"];
-  //const char* password = doc["password"];
-  
-  connectWifi();
-
-  connectDB();
-  
-  startDNS();
-  
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, lectura);
-  });
-  
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-
-  server.on("/modifyWifi", HTTP_GET, [](AsyncWebServerRequest *request){
-    String ssid;
-    String password;
-    if (request->hasParam("ssid")){
-      ssid = request->getParam("ssid")->value();
-    }
-    else{
-      ssid = "No se ha introducido SSID";
-    }
-    if (request->hasParam("password")){
-      password = request->getParam("password")->value();
-    }
-    else{
-      password = "No se ha introducido contraseña";
-    }
-    modifyWifi(ssid, password);
-    request->send(SPIFFS, "/index.html", String(), false, lectura);
-  });
-  server.on("/modifyLimits", HTTP_GET, [](AsyncWebServerRequest *request){
-    String co2_max;
-    String co2_mid;
-    String distance;
-    if (request->hasParam("co2_max")){
-      co2_max = request->getParam("co2_max")->value();
-    }
-    else{
-      co2_max = "No se ha introducido CO2_MAX";
-    }
-    if (request->hasParam("co2_mid")){
-      co2_mid = request->getParam("co2_mid")->value();
-    }
-    else{
-      co2_mid = "No se ha introducido CO2_MID";
-    }
-    if (request->hasParam("distance")){
-      distance = request->getParam("distance")->value();
-    }
-    else{
-      distance = "No se ha introducido distancia";
-    }
-    modifyLimits(co2_max, co2_mid, distance);
-    request->send(SPIFFS, "/index.html", String(), false, lectura);
-  });
-  server.on("/modifyStudent", HTTP_GET, [](AsyncWebServerRequest *request){
-    String dni;
-    
-    if (request->hasParam("dni")){
-      dni = request->getParam("dni")->value();
-    }
-    else{
-      dni = "No se ha introducido SSID";
-    }
-    
-    modifyStudent(dni);
-    request->send(SPIFFS, "/index.html", String(), false, lectura);
-  });
-
-  
-  // Start server
-  server.begin();
-
-  ///////////////
-  
-  //PANTALLA
+  ///////PANTALLA
   if(!display.begin(SSD1306_SWITCHCAPVCC)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
@@ -315,39 +287,91 @@ void setup() {
   
   display.clearDisplay();
 
-  display.drawBitmap(0, 0, myBitmap, 128, 64, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
+  display.drawBitmap(0, 0, logoUCLM, 128, 64, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
   display.display();
-  delay(2000); // Pause for 2 seconds
+  //delay(2000); // Pause for 2 seconds
 
-  display.clearDisplay();
-  display.display();
+  
+  ////////
+  ////////////////////////
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin(true)){
+    Serial.println("Error al montar el sistema de ficheros SPIFFS");
+    return;
+  }
+  
+  createAccessPoint();
+  
+  // Connect to Wi-Fi
+  //const char* ssid = doc["ssid"];
+  //const char* password = doc["password"];
+  
+  connectWifi();
+
+  connectDB();
+  
+  startDNS();
+  
+  
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  setupServer();
+
+  DynamicJsonDocument doc = readConFile();
+  conf_distance = doc["distance"];
+  conf_co2_mid = doc["co2_mid"];
+  conf_co2_max = doc["co2_max"];
+  String cambio = doc["dni"];
+  conf_dni = cambio;
+  ///////////////
+  ////////
+  xTaskCreatePinnedToCore(
+      checkConnections, /* Function to implement the task */
+      "TaskCheckConnections", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &TaskCheckConnections,  /* Task handle. */
+      0); /* Core where the task should run */
+
+  ////////
   
   // Sensor proximidad
   sensor = new Simple_HCSR04(ECHO_PIN, TRIG_PIN);
-  
+
+  display.clearDisplay();
+  display.display();
 }
 
 void loop() {
-
+  dnsServer.processNextRequest();
   // Lectura CO2
-  int medicion = analogRead(PIN_SENSOR);
+  int medicionCO2 = analogRead(PIN_SENSOR);
   Serial.print("Medicion CO2:");
-  Serial.println(medicion);
+  Serial.println(medicionCO2);
 
   //Lectura Distancia
-  unsigned long distance = sensor->measure()->cm();
+  unsigned long medicionDistancia = sensor->measure()->cm();
   Serial.print("distance: ");
-  Serial.print(distance);
+  Serial.print(medicionDistancia);
   Serial.print("cm\n");
 
   String mensaje;
-  if (distance < 150)
+  if (medicionDistancia < conf_distance)
   {
     mensaje = "Demasiado\ncerca";
   }
-  else
-  {
-    mensaje = "Distancia\ncorrecta";
+  //else
+  //{
+  //  mensaje = "Distancia\ncorrecta";
+  //}
+  if (medicionCO2 > conf_co2_mid && medicionCO2 < conf_co2_max){
+    mensaje = "Aire\ncargandose";
+  }
+  if (medicionCO2 > conf_co2_max){
+    mensaje = "Alta\nconcentracion CO2";
   }
   //inicializar pantalla
   display.setTextSize(1);
@@ -366,7 +390,7 @@ void loop() {
   Serial.println(rssi);
   Serial.println("");
   int bars;
-  //display.fillRect(100,13,3,3,WHITE);
+  //display.fillRect(80,13,3,3,WHITE);
   //display.fillRect(105,10,3,6,WHITE);
   //display.fillRect(110,7,3,9,WHITE);
   //display.fillRect(115,4,3,12,WHITE);
@@ -390,23 +414,35 @@ void loop() {
     display.fillRect(95 + (b * 5), 10 - (b * 2), 3, b * 2, WHITE);
   }
 
+  if(conn.connected() == 1){
+      display.drawBitmap(85, 0, database_icon, 10, 10, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
+  }
+  //else{
+  //  Serial.println("Desconectado de la DB. Intentando conectar...");
+  //  connectDB();
+  //}
+
   display.setTextColor(WHITE);
   display.setTextSize(1);
 
   display.setTextSize(2);             // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 25);
-  display.print(medicion);
+  display.print(medicionCO2);
 
   display.setCursor(0,40);
   display.setTextSize(1); 
   display.println("Distancia:");
   display.setCursor(0,50);
   display.setTextSize(2);
-  display.print(distance);
-  
+  display.print(medicionDistancia);
+
   display.display();
 
+  
+
+  insertMedicionDB(medicionCO2, conf_dni);
+ 
   delay(5000); //Actualización cada 5 segundos
 }
 
@@ -456,13 +492,25 @@ void connectWifi(){
   DynamicJsonDocument doc = readConFile();
   const char* ssid = doc["ssid"];
   const char* password = doc["password"];
+  Serial.print("SSID: ");
+  Serial.print(ssid);
+  Serial.print(", PASSWORD: ");
+  Serial.println(password);
   WiFi.begin(ssid, password);
+  int intento = 0;
   while (WiFi.status() != WL_CONNECTED) {
+    if (intento == 10){
+      break;
+    }
     delay(1000);
     Serial.println("Conectando WiFi..");
+    intento++;
   }
+  semaforo = true;
 }
 void startDNS(){
+  //Serial.println("Starting DNS Server");
+  //dnsServer.start(53, "*", WiFi.softAPIP());
   DynamicJsonDocument doc = readConFile();
   String dni = doc["dni"];
   String hostname = "arduino_" + dni;
@@ -472,5 +520,191 @@ void startDNS(){
   if(!MDNS.begin(hostname.c_str())) {
      Serial.println("Error al iniciar mDNS");
      return;
+  }
+}
+
+String scanWiFi(){
+  //WiFi.mode(WIFI_STA);
+  //WiFi.disconnect();
+    //delay(100);
+  Serial.println("Iniciando escaneo WiFi...");
+  // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    String redes = "[";
+    Serial.println("Escaneo finalizado.");
+    if (n == 0) {
+        Serial.println("No se han encontrado redes.");
+    } else {
+        Serial.print(n);
+        Serial.println(" redes encontradas");
+        for (int i = 0; i < n; ++i) {
+          if(!(WiFi.SSID(i) == "")){
+            if (!(i == 0)){
+               redes = redes + ",";
+            }
+            redes = redes + '"' + WiFi.SSID(i) + '"';
+            
+          }
+          
+          
+            // Print SSID and RSSI for each network found
+            //Serial.print(i + 1);
+            //Serial.print(": ");
+            //Serial.print(WiFi.SSID(i));
+            //Serial.print(" (");
+            //Serial.print(WiFi.RSSI(i));
+            //Serial.print(")");
+            //Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            //delay(10);
+        }
+        redes = redes + "]";
+    }
+    Serial.print("JSON completo:");
+    Serial.println(redes);
+
+    return redes;
+    // Wait a bit before scanning again
+    //delay(5000);
+}
+void setupServer(){
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, lectura);
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  server.on("/modifyWifi", HTTP_GET, [](AsyncWebServerRequest *request){
+    String ssid;
+    String password;
+    if (request->hasParam("ssid")){
+      ssid = request->getParam("ssid")->value();
+    }
+    else{
+      ssid = "No se ha introducido SSID";
+      Serial.println("No se ha introducido SSID");
+    }
+    if (request->hasParam("password")){
+      password = request->getParam("password")->value();
+    }
+    else{
+      password = "No se ha introducido contraseña";
+    }
+
+    modifyWifi(ssid, password);
+    request->redirect("http://arduino_" + conf_dni + "/");
+    //request->send(SPIFFS, "/index.html", String(), false, lectura);
+  });
+  server.on("/modifyLimits", HTTP_GET, [](AsyncWebServerRequest *request){
+    String co2_max;
+    String co2_mid;
+    String distance;
+    if (request->hasParam("co2_max")){
+      co2_max = request->getParam("co2_max")->value();
+    }
+    else{
+      co2_max = "No se ha introducido CO2_MAX";
+    }
+    if (request->hasParam("co2_mid")){
+      co2_mid = request->getParam("co2_mid")->value();
+    }
+    else{
+      co2_mid = "No se ha introducido CO2_MID";
+    }
+    if (request->hasParam("distance")){
+      distance = request->getParam("distance")->value();
+    }
+    else{
+      distance = "No se ha introducido distancia";
+    }
+    modifyLimits(co2_max, co2_mid, distance);
+    request->redirect("http://arduino_" + conf_dni + "/");
+    //request->send(SPIFFS, "/index.html", String(), false, lectura);
+  });
+  server.on("/modifyStudent", HTTP_GET, [](AsyncWebServerRequest *request){
+    String dni;
+    String nombre;
+    String apellidos;
+    String password;
+    
+    if (request->hasParam("dni")){
+      dni = request->getParam("dni")->value();
+    }
+    if (request->hasParam("nombre")){
+      nombre = request->getParam("nombre")->value();
+    }
+    if (request->hasParam("apellidos")){
+      apellidos = request->getParam("apellidos")->value();
+    }
+    if (request->hasParam("alu_password")){
+      password = request->getParam("alu_password")->value();
+    }
+    
+    
+    modifyStudent(dni, nombre, apellidos, password);
+    delay(2000);
+    request->redirect("http://arduino_" + conf_dni + "/");
+    //request->send(SPIFFS, "/index.html", String(), false, lectura);
+  });
+  server.on("/scanWiFi", HTTP_GET, [](AsyncWebServerRequest *request){
+    String redes = scanWiFi();
+    request->send(200, "application/json", redes);
+  });
+
+  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
+  // Start server
+  server.begin();
+}
+
+void insertMedicionDB(int medicionCO2 , String alumno){
+  
+  String sentencia = "INSERT INTO TFG_ARDUINO.medicion (co2, fecha, alumno) VALUES ('" + (String) medicionCO2 + "', now(), '" + alumno + "')";
+  int str_len = sentencia.length() + 1; //Length (with one extra character for the null terminator)
+  char INSERT_SQL[str_len];
+  sentencia.toCharArray(INSERT_SQL, str_len);
+  
+  // Initiate the query class instance
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+  // Execute the query
+  cur_mem->execute(INSERT_SQL);
+  // Note: since there are no results, we do not need to read any data
+  // Deleting the cursor also frees up memory used
+  delete cur_mem;
+
+}
+void updateAlumnoDB(String dni, String nombre, String apellidos, String password, String conf_dni){
+  String sentencia = "UPDATE TFG_ARDUINO.alumno SET dni = '" + dni + "', nombre = '" + nombre + "', apellidos = '" + apellidos + "', password = '" + password + "' WHERE dni = '" + conf_dni + "'";
+  int str_len = sentencia.length() + 1; //Length (with one extra character for the null terminator)
+  char UPDATE_SQL[str_len];
+  sentencia.toCharArray(UPDATE_SQL, str_len);
+  
+  // Initiate the query class instance
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+  // Execute the query
+  cur_mem->execute(UPDATE_SQL);
+  // Note: since there are no results, we do not need to read any data
+  // Deleting the cursor also frees up memory used
+  delete cur_mem;
+}
+
+void checkConnections( void * parameter){
+  for(;;){
+    if(semaforo){
+      //Comprueba si esta conectado a la red WiFi.
+      if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Desconectado de la red WiFi. Intentando conectar...");
+        connectWifi();
+      }
+      //Comprueba si esta conectado a la base de datos.
+      if(conn.connected() == 0 && WiFi.status() == WL_CONNECTED){
+        Serial.println("Desconectado de la DB. Intentando conectar...");
+        connectDB();    
+      }
+    }
+    
+    delay(5000);
   }
 }
